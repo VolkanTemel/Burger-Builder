d3d5773575d81ed4a919e1e55b4ab0da0b5a9e9b
Volkan Temel <sapphiron07@windowslive.com>
2020-11-27 16:04:23 +0300

    Initial commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+const { assertCompletionStatement } = require("babel-types");
+import React, { Component } from 'react';
+import Aux from '..//../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+           <Aux>
+               <div>Burger</div>
+               <div>Build Controls</div>
+           </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
