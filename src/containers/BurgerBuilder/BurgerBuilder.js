ac728a9049c069de0f6fdc96b25dd71637f941a2
Volkan Temel <sapphiron07@windowslive.com>
2020-12-03 23:26:26 +0300

    Enter

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Auxiliary';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
@@ -26,54 +26,54 @@ class BurgerBuilder extends Component {
         purchasing: false
     }
 
-    updatePurchaseState (ingredients) {
-        const sum = Object.keys( ingredients )
-        .map( igKey => {
-            return ingredients[igKey];
-        } )
-        .reduce( ( sum, el ) => {
-            return sum + el;
-        }, 0);
-        this.setState({purchasable: sum > 0});
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[ igKey ];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({ purchasable: sum > 0 });
     }
 
     addIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
+        const oldCount = this.state.ingredients[ type ];
         const updatedCount = oldCount + 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
-        updatedIngredients[type] = updatedCount;
-        const priceAddition = INGREDIENT_PRICES[type];
+        updatedIngredients[ type ] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[ type ];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
         this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-        if( oldCount <= 0 ){
+        const oldCount = this.state.ingredients[ type ];
+        if (oldCount <= 0) {
             return;
         }
         const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
-        updatedIngredients[type] = updatedCount;
-        const priceDeduction = INGREDIENT_PRICES[type];
+        updatedIngredients[ type ] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[ type ];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
         this.updatePurchaseState(updatedIngredients);
     }
 
     purchaseHandler = () => {
-        this.setState({purchasing: true});
+        this.setState({ purchasing: true });
     }
 
     purchaseCancelHandler = () => {
-        this.setState({purchasing: false});
+        this.setState({ purchasing: false });
     }
 
     purchaseContinueHandler = () => {
@@ -85,27 +85,27 @@ class BurgerBuilder extends Component {
             ...this.state.ingredients
         };
         for (let key in disabledInfo) {
-            disabledInfo[key] = disabledInfo[key] <= 0
+            disabledInfo[ key ] = disabledInfo[ key ] <= 0
         }
         return (
-           <Aux>
-               <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredients}
-                        purchaseCanceled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler}
-                        price={this.state.totalPrice} />
-               </Modal>
-               <Burger 
-                    ingredients={this.state.ingredients} />
-               <BuildControls 
-                    ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo} 
-                    purchasable={this.state.purchasable}
-                    ordered={this.purchaseHandler}
-                    price={this.state.totalPrice} />
-           </Aux>
+            <Aux>
+                <Modal show={ this.state.purchasing } modalClosed={ this.purchaseCancelHandler }>
+                    <OrderSummary
+                        ingredients={ this.state.ingredients }
+                        purchaseCanceled={ this.purchaseCancelHandler }
+                        purchaseContinued={ this.purchaseContinueHandler }
+                        price={ this.state.totalPrice } />
+                </Modal>
+                <Burger
+                    ingredients={ this.state.ingredients } />
+                <BuildControls
+                    ingredientAdded={ this.addIngredientHandler }
+                    ingredientRemoved={ this.removeIngredientHandler }
+                    disabled={ disabledInfo }
+                    purchasable={ this.state.purchasable }
+                    ordered={ this.purchaseHandler }
+                    price={ this.state.totalPrice } />
+            </Aux>
         );
     }
 }
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import Aux from "../../hoc/Auxiliary";
+import Aux from "../Auxiliary/Auxiliary";
 import classes from "./Layout.css";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
   };
   sideDrawerClosedHandler = () => {
     this.setState({ showSideDrawer: false });
@@ -21,12 +21,12 @@ class Layout extends Component {
   render() {
     return (
       <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <Toolbar drawerToggleClicked={ this.sideDrawerToggleHandler } />
         <SideDrawer
-          open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler}
+          open={ this.state.showSideDrawer }
+          closed={ this.sideDrawerClosedHandler }
         />
-        <main className={classes.Content}>{this.props.children}</main>
+        <main className={ classes.Content }>{ this.props.children }</main>
       </Aux>
     );
   }
